7b2638872d95abd265a5bf4fbaefc007dcb5fd17
Julius Sipos <julius.sipos98@gmail.com>
2023-01-04 09:13:46 +0100

    Header Template edit

--- template-parts/header/header-03.php
+++ template-parts/header/header-03.php
@@ -52,10 +52,7 @@
 					<div class="col-sm-12">
 						<div class="page-header-bottom-inner">
 							<div class="row">
-								<div class="col-xl-3 col-sm-6">
-									<?php Medizin_Header::instance()->print_category_dropdown(); ?>
-								</div>
-								<div class="col-xl-6 page-navigation-wrap">
+								<div class="col-xl-9 page-navigation-wrap">
 									<?php get_template_part('template-parts/navigation'); ?>
 								</div>
 								<div class="col-xl-3 col-sm-6">
