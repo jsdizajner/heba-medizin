9b9655247a0e40c56d212b8fa82c9d4f5e0d659d
Julius Sipos <julius.sipos98@gmail.com>
2023-06-08 15:29:02 +0200

    HOTFIX: Translate and adc image error on single product

--- functions.php
+++ functions.php
@@ -51,7 +51,7 @@ function bbloomer_attach_pdf_to_emails($attachments, $email_id, $order, $email)
 /**
  * @snippet Load ADC Title before WooCommerce Title
  */
-add_filter( 'the_title', 'load_adc_title_first', 10, 2 );
+//add_filter( 'the_title', 'load_adc_title_first', 10, 2 );
 function load_adc_title_first( $title, $id = null ) {
 	$post = get_post($id);
 	if ($post->post_type == 'product' and get_post_meta($id,'_importer_supplier', true) == 'Medart' ) {
